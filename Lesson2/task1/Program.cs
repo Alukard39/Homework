40b6641c3094d4fd5d0999f6de30219f014293b3
Dmitry <alukard39@gmail.com>
2021-01-25 10:19:07 +0300

    task1

---
+++ Lesson2/task1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter today minimal temperature in C!");
+            string minT = Console.ReadLine();
+            Console.WriteLine("Please enter today maximal temperature in C!");
+            string maxT = Console.ReadLine();
+            int avT = (Convert.ToInt32(maxT) + Convert.ToInt32(minT)) / 2;
+            Console.WriteLine($"The avarage daily Temperature is {avT}");
+        }
+    }
+}
