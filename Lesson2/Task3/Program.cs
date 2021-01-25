c5f8613cb1c8aa12477eaaf3361ee1d6f72a5b69
Dmitry <alukard39@gmail.com>
2021-01-25 10:54:44 +0300

    Task3

---
+++ Lesson2/Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the number!");
+            string snumber = Console.ReadLine();
+            int inumber = Convert.ToInt32(snumber);
+            if (inumber % 2 == 0)
+            {
+                Console.WriteLine("Your number is Even");
+            }
+            else {
+                Console.WriteLine("Your number is Odd");
+            }
+
+        }
+    }
+}
