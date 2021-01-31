da80bfe4a61de0a8c9e71836af8cefc8dba6cc5e
Dmitry <alukard39@gmail.com>
2021-01-31 03:33:23 +0300

    done

---
+++ Lesson 3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = new int[5, 5]
+            {
+                {01, 02, 03, 04, 05 },
+                {11, 12, 13, 14, 15 },
+                {21, 22, 23, 24, 25 },
+                {31, 32, 33, 34, 35 },
+                {41, 42, 43, 44, 45 }
+            };
+            for(int j = 0, i = 0;i < 5;i++, j++)
+                {
+                    Console.Write($"{array[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+         
+    }
---
+++ Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a word:");
+            string input = Console.ReadLine();
+
+            for (int i = input.Length; i != 0; i--)
+            {
+                char c = input[i-1];
+                Console.Write(c);
+            }
+            Console.WriteLine();
+        }
+    }
+}
